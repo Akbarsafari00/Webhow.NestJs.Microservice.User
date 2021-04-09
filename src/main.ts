aa185a0838a29f0f7db44701d17d79680ee4d525
Akbar Ahmadi Saray <akbarahmadi@utechia.com>
2021-04-09 23:34:28 +0430

    init

---
+++ src/main.ts
@@ -0,0 +1,20 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import {Transport} from "@nestjs/microservices";
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  app.connectMicroservice({
+    transport: Transport.RMQ,
+    options: {
+      urls: [process.env.RMQ_HOST],
+      queue: process.env.RMQ_QUEUE,
+      queueOptions: {
+        durable: Boolean(process.env.RMQ_DURABLE)
+      },
+    },
+  });
+  await app.listen(3000);
+}
+bootstrap();
